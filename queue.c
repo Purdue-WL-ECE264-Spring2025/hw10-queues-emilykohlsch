abf792a0049397ee49aa6c177c67293b1f4824a6
Emily Jesslyn Kohlschreiber <ekohlsch@ececomp3.ecn.purdue.edu>
2025-04-04 23:22:27 -0400

    6th commit

--- queue.c
+++ queue.c
@@ -2,90 +2,68 @@
 #include "tile_game.h"
 #include <stdlib.h>
 
-struct list_node *new_node(struct game_state state);
 
 void enqueue(struct queue *q, struct game_state state) {
-  struct list_node *node = new_node(state);
-    if (!node) return;
-
-    if (!q->data.head) {
-        q->data.head = node;
-    } else {
-        struct list_node *temp = q->data.head;
-        while (temp->next) {
-            temp = temp->next;
-        }
-        temp->next = node;
-    }
+  size_t encoded = serialize(state);
+  insert_at_tail(&q->data, encoded);
 }
 
 int is_solved(struct game_state state) {
-    int solved[4][4] = {
+    int goal[4][4] = {
         {1, 2, 3, 4},
         {5, 6, 7, 8},
         {9, 10, 11, 12},
         {13, 14, 15, 0}
     };
-
-    for (int row = 0; row < 4; row++) {
-        for (int col = 0; col < 4; col++) {
-            if (state.tiles[row][col] != solved[row][col]) {
-                return 0;  // Not solved
-            }
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
+            if (state.tiles[r][c] != goal[r][c]) return 0;
         }
     }
-
-    return 1;  // Solved
+    return 1;
 }
 
 struct game_state dequeue(struct queue *q) { 
-   if (!q->data.head) {
-        exit(1);  // Handle the case where the queue is empty
-    }
-
-    struct list_node *temp = q->data.head;
-    struct game_state state = deserialize(temp->value); 
-    q->data.head = q->data.head->next;
-    free(temp);
-
-    return state;
+   size_t encoded = remove_from_head(&q->data);  // uses linked_list function
+   return deserialize(encoded);
 }
 
 int number_of_moves(struct game_state start) { 
    struct queue q = {0};
-    enqueue(&q, start);
-    
-    while (q.data.head) {
-        struct game_state current = dequeue(&q);
-
-        if (is_solved(current)) {
-            return current.num_steps;
+   enqueue(&q, start);   
+   
+   while (q.data.head) {   
+        struct game_state current = dequeue(&q); 
+        
+        if (is_solved(current)) { 
+            return current.num_steps;  
         }
 
-        struct game_state new_state = current;
+        struct game_state new_state = current;  
 
+       
         if (current.empty_row < 3) {
             new_state = current;
             move_up(&new_state);  
-            enqueue(&q, new_state);
+            enqueue(&q, new_state);  
         }
 
         if (current.empty_row > 0) {
             new_state = current;
             move_down(&new_state);  
-            enqueue(&q, new_state);
+            enqueue(&q, new_state);  
         }
 
         if (current.empty_col < 3) {
             new_state = current;
             move_left(&new_state);
-            enqueue(&q, new_state);
+            enqueue(&q, new_state);  
         }
 
         if (current.empty_col > 0) {
             new_state = current;
             move_right(&new_state);
-            enqueue(&q, new_state);
+            enqueue(&q, new_state); 
         }
     }
     
